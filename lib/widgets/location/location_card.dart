74d7e8141da299a3a530471be8405614c1df0f49
Daniel Mercado <dms9812@hotmail.com>
2023-08-25 16:24:58 -0500

    location style card

--- lib/widgets/location/location_card.dart
+++ lib/widgets/location/location_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/models/location_model.dart';
+import 'package:rick_and_morty/theme/my_colors.dart';
 
 class LocationCard extends StatelessWidget {
   final LocationModel character;
@@ -12,12 +13,26 @@ class LocationCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Text(character.dimension),
-          Text(character.name),
-        ],
+      padding: const EdgeInsets.only(left: 44, right: 44, top: 3, bottom: 3),
+      child: Card(
+        color: MyMaterialColor.myColor[300],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(character.name, style: const TextStyle(fontSize: 24),),
+              const SizedBox(height: 10,),
+              Text('Tipo: ${character.type}', style: const TextStyle(fontSize: 16, fontFamily: 'Inder')), 
+              const SizedBox(height: 10,),
+              Text('Dimension: ${character.dimension}', style: const TextStyle(fontSize: 16, fontFamily: 'Inder')),
+              const SizedBox(height: 10,),
+              Text('Residentes: ${character.residents.length.toString()}' , style: const TextStyle(fontSize: 16, fontFamily: 'Inder')),
+            ],
+          ),
+        ),
       ),
     );
   }
