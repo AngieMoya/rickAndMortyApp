941df3e5dd0b345bad0666b13920de338a43f7fb
Daniel Mercado <dms9812@hotmail.com>
2023-08-25 14:22:49 -0500

    my app to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:rick_and_morty/pages/my_app.dart';
+import 'package:rick_and_morty/routes/routes.dart';
+import 'package:rick_and_morty/theme/my_colors.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: MyMaterialColor.myColor, fontFamily: 'Jolly Lodger'),
+      initialRoute: '/',
+      routes: routes,
+    );
+  }
 }
